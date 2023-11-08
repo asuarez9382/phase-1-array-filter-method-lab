20e7d8f10a2095c2bd084a1b627bc423d6288319
Adrian Suarez <asuarez9382@gmail.com>
2023-11-08 12:29:29 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(arr, match) {
+    const names = arr.filter(element => element.toLowerCase() === match.toLowerCase())
+    return names;
+}
+
+function fuzzyMatch(arr, match) {
+    const names = arr.filter(element => element.slice(0,match.length) === match)
+    return names;
+} 
+    
+function matchName(arr, match) {
+    const names = arr.filter(element => element.name === match)
+    return names;
+}
